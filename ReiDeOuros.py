217bbbfae58e6b3fe25b8c9d3d0b5d597ce9e638
matheuhst <mmmatheus.fsouza@gmail.com>
2026-03-16 06:54:47 -0300

    Renomeei o nome do arquivo par aa sua versão final

--- ReiDeOuros.py
+++ ReiDeOuros.py
@@ -1,4 +1,5 @@
 from time import sleep
+from random import randint, choice
 
 
 def encontrar_numero_mais_proximo(media, numeros):
@@ -12,6 +13,16 @@ def encontrar_numero_mais_proximo(media, numeros):
             numero_mais_proximo = numero
 
     return numero_mais_proximo
+
+
+def jogador_virtual():
+    estrategia = choice([
+        lambda: randint(0, 100),
+        lambda: randint(35, 45),
+        lambda: randint(20, 30),
+        lambda: randint(10, 20)
+    ])
+    return estrategia()
 
 
 print('Jogo: ')
@@ -30,17 +41,20 @@ Os restantes perderão 1 ponto dos 10 que são concebidos. Se um jogador chegar 
 \033[92mBoa sorte.\033[m''')
 
 dados = []
-jogadores = []
 pontos = 0
 rodada = 1
 
-for i in range(0, 5):
-    jogadores.append(str(input(f'\033[4;91mK♦\033[m Nome do \033[91mJogador {i + 1}\033[m: ').capitalize().strip()))
-    dados.append([jogadores[i], pontos])
+num_humanos = int(input("Quantos jogadores humanos? "))
 
-mostrou_1 = False
-mostrou_2 = False
-mostrou_3 = False
+for i in range(num_humanos):
+    nome = input(f'Nome do Jogador {i+1}: ').capitalize().strip()
+    dados.append([nome, 0, False])
+
+while len(dados) < 5:
+    nome = f"Bot{len(dados)+1}"
+    dados.append([nome, 0, True])
+
+mostrou_1 = mostrou_2 = mostrou_3 = False
 
 while True:
     numeros = []
@@ -48,7 +62,9 @@ while True:
     print()
     print(f'Rodada {rodada}... Comecem.')
     sleep(2)
+
     while True:
+
         if len(dados) <= 4 and not mostrou_1:
             mostrou_1 = True
             print()
@@ -72,20 +88,28 @@ a penalidade do perdedor será dobrada. \033[4;91mK♦\033[m''')
             print('\033[4;91mK♦\033[m Se alguém escolher 0, o jogador que escolher 100 é o vencedor. \033[4;91mK♦\033[m')
             print()
 
-        if dados[n][-1] >= -10:
-            num_jog = int(input(f'Mestre \033[4;97m{dados[n][0]}\033[m informe seu número: '))
+        if dados[n][1] >= -10:
+
+            if dados[n][2]:  # BOT
+                num_jog = jogador_virtual()
+                print(f'Mestre \033[4;97m{dados[n][0]}\033[m informe seu número: {num_jog}')
+            else:
+                num_jog = int(input(f'Mestre \033[4;97m{dados[n][0]}\033[m informe seu número: '))
+
             if num_jog > 100 or num_jog < 0:
                 print()
                 print('\033[4;91mK♦\033[m \033[4;93mApenas é válido um número de 0 a 100.\033[m \033[4;91mK♦\033[m')
                 continue
             else:
                 numeros.append(num_jog)
+
             n += 1
+
             if len(numeros) == len(dados):
                 break
+
         else:
             continue
-
 
     for i in range(0, len(dados)):
         dados[i].append(numeros[i])
@@ -98,63 +122,37 @@ a penalidade do perdedor será dobrada. \033[4;91mK♦\033[m''')
     print('Vamos ver o que cada jogador escolheu:')
     print()
     sleep(3)
-    for i, k, j in dados:
-        print(f'Mestre {i.ljust(10, ".")}\033[4;96m{j:>0}\033[m')
+
+    for jogador in dados:
+        nome = jogador[0]
+        numero = jogador[-1]
+        print(f'Mestre {nome.ljust(10, ".")}\033[4;96m{numero:>0}\033[m')
+
     sleep(2)
+
     print()
     print(f'A média de todos os números foi de {media}')
     print(f'O resultado de {media} multiplicado por 0.8 corresponde a {resultado:.1f}')
 
     n = 0
     sleep(4)
-    for i, k, j in dados:
+
+    for jogador in dados:
+        i = jogador[0]
+        k = jogador[1]
+        j = jogador[-1]
+
         if j == numero_encontrado and not len(dados) == 2:
             vencedor = i
             print(f'O Vencedor da rodada é \033[4;33mMestre {vencedor}\033[m')
+
         else:
-            if len(dados) <= 4:
-                dicionario = {}
-                for item in dados:
-                    nome, numero = item
-                    if numero not in dicionario:
-                        dicionario[numero] = []
-                    dicionario[numero].append(nome)
-
-                resultados = {numero: nomes for numero, nomes in dicionario.items() if len(nomes) > 1}
-
-                if resultados:
-                    for numero, nomes in resultados.items():
-                        print(f'Numero {numero}: {" e ".join(nomes)}')
-
-                else:
-                    pass
 
             if len(dados) <= 3:
                 dados[n][1] -= 2
                 pontos = dados[n][1]
             else:
                 dados[n][1] -= 1
-                pontos = dados[n][1]
-
-        if len(dados) == 2:
-            if dados[0][-1] == 100 and dados[1][-1] == 100:
-                dados[n][1] -= 2
-
-            elif dados[0][-1] == 0 and dados[1][-1] == 0:
-                dados[n][1] -= 2
-
-            elif dados[0][-1] == 100 and dados[1][-1] == 0:
-                print(f'O Vencedor da rodada é \033[4;33mMestre {dados[0][0]}\033[m')
-
-            elif dados[0][-1] == 0 and dados[1][-1] == 100:
-                print(f'O Vencedor da rodada é \033[4;33mMestre {dados[1][0]}\033[m')
-
-            elif j == numero_encontrado:
-                vencedor = i
-                print(f'O Vencedor da rodada é \033[4;33mMestre {vencedor}\033[m')
-
-            else:
-                dados[n][1] -= 2
                 pontos = dados[n][1]
 
         n += 1
@@ -166,16 +164,15 @@ a penalidade do perdedor será dobrada. \033[4;91mK♦\033[m''')
     for i in range(0, len(dados)):
         del dados[i][-1]
 
-    for i, k in dados:
-        print(f'{i.ljust(15, ".")}[ \033[4;33m{k:>0}\033[m ]')
+    for jogador in dados:
+        print(f'{jogador[0].ljust(15, ".")}[ \033[4;33m{jogador[1]:>0}\033[m ]')
 
     num = 0
-    for i, k in dados:
-        if k <= -10:
+
+    for jogador in dados:
+        if jogador[1] <= -10:
             del dados[num]
-            print(dados)
             num -= 1
-            continue
         num += 1
 
     if len(dados) == 1:
